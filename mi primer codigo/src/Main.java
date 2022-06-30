59c75da4452db98453d2889b401e1aace0ceae2a
Cristian Baez <cristianbaez1983@gmail.com>
2022-06-29 20:37:31 -0400

    Mi primer codigo Java

---
+++ mi primer codigo/src/Main.java
@@ -0,0 +1,30 @@
+import com.sun.nio.sctp.NotificationHandler;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String escrivirNombre = "Cati";
+
+        if (escrivirNombre == "Selva"){
+            System.out.println("Selva");
+        }
+
+        else if  (escrivirNombre == "Noah") {
+            System.out.println("Noah");
+        }
+        else  if (escrivirNombre == "Cati") {
+            System.out.println("Cati");
+
+        } else if (escrivirNombre == "Vero"){
+            System.out.println("Vero");
+
+        } else {
+            System.out.println("Cristian");
+        }
+
+
+
+
+
+    }
+}
